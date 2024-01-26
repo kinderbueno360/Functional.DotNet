67979eb78b8ee62524ebbcda6f8739ca375646a1
Carlos Bueno <carlos.bueno@csl.lu>
2024-01-26 17:17:32 +0100

    Add Enum Parse from string to Option of Enum

---
+++ src/Functional.DotNet/Extensions/EnumExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional.DotNet.Extensions
+{
+    using static F;
+
+    public static class EnumExt
+    {
+        public static Option<T> Parse<T>(this string s) where T : struct
+           => Enum.TryParse(s, out T t)
+                ? Some(t)
+                : None;
+    }
+}
